d463de5424037e3e786144d72c9254a0246731e8
Stéphane Francel <stephane.francel@gmail.com>
2022-12-06 00:37:13 +0100

    Add prettier-plugin-organize-imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
 
