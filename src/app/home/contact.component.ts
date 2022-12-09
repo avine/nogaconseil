7c2bfdd8cc09be7611b932b8699f500c1ee00c58
Stéphane Francel <stephane.francel@gmail.com>
2022-12-09 14:00:20 +0100

    Add "Janie" name and some icons

--- src/app/home/contact.component.ts
+++ src/app/home/contact.component.ts
@@ -5,7 +5,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
   standalone: true,
   encapsulation: ViewEncapsulation.None,
   template: `
-    <h3 class="h2">Me contacter</h3>
+    <img src="assets/reshot-icon-mentoring.svg" class="icon" alt="" />
+
+    <h3 class="h2">
+      <div class=" fg-secondary">Me contacter</div>
+      Janie Harros
+    </h3>
 
     <p class="h4 thin">
       <a href="tel:+33665209003" rel="noopener noreferrer" class="big-icon big-icon--tel">06 65 20 90 03</a>
--- src/app/home/landing.component.ts
+++ src/app/home/landing.component.ts
@@ -5,7 +5,10 @@ import { Component, ViewEncapsulation } from '@angular/core';
   standalone: true,
   encapsulation: ViewEncapsulation.None,
   template: `
-    <h1 class="h5 thin">Noga Conseil</h1>
+    <h1 class="h5 thin">
+      <img src="assets/reshot-icon-biology.svg" class="icon" alt="" style="margin-right: 0.5rem;" />
+      Noga Conseil
+    </h1>
 
     <h2 class="h1">
       <div class="fg-secondary">Apprenez plus facilement</div>
