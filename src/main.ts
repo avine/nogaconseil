07eec6962a2fd2fcf4dcbbd6fe58692ba2b8f92e
Stéphane Francel <stephane.francel@gmail.com>
2022-12-12 00:54:06 +0100

    Enable scrollPositionRestoration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { APP_ROUTES } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(APP_ROUTES)],
+  providers: [provideRouter(APP_ROUTES, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }))],
 }).catch((err) => console.error(err));
