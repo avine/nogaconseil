7c1686bae2b11120df16688457a09074c94e24a9
Stéphane Francel <stephane.francel@gmail.com>
2022-12-18 06:10:18 +0100

    Only add "/typo" route in dev mode

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
+
+import { environment } from '../environments/environment';
 import { HomeComponent } from './home/home.component';
-import { CnvComponent } from './pages/cnv.component';
-import { EtreAttentifComponent } from './pages/etre-attentif.component';
-import { FacilitationVisuelleComponent } from './pages/facilitation-visuelle.component';
-import { NeurosciencesComponent } from './pages/neurosciences.component';
-import { PeiComponent } from './pages/pei.component';
 import { TypoComponent } from './typo/typo.component';
 
 export const APP_ROUTES: Routes = [
@@ -13,31 +10,14 @@ export const APP_ROUTES: Routes = [
     component: HomeComponent,
   },
   {
-    path: 'pei',
-    component: PeiComponent,
-  },
-  {
-    path: 'facilitation-visuelle',
-    component: FacilitationVisuelleComponent,
-  },
-  {
-    path: 's-entrainer-a-etre-attentif',
-    component: EtreAttentifComponent,
-  },
-  {
-    path: 'neurosciences',
-    component: NeurosciencesComponent,
-  },
-  {
-    path: 'cnv',
-    component: CnvComponent,
-  },
-  {
-    path: 'typo',
-    component: TypoComponent,
-  },
-  {
     path: '**',
     redirectTo: '/',
   },
 ];
+
+if (!environment.production) {
+  APP_ROUTES.unshift({
+    path: 'typo',
+    component: TypoComponent,
+  });
+}
