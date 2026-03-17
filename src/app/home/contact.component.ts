570ccc056a374e3548c4c92c8a120c59dad9d3ab
Stéphane Francel <stephane.francel@zenika.com>
2026-03-17 12:44:30 +0100

    Add "buy me a coffee" link

--- src/app/home/contact.component.ts
+++ src/app/home/contact.component.ts
@@ -20,6 +20,15 @@ import { Component, ViewEncapsulation } from '@angular/core';
         jharros&commat;gmail.com
       </a>
     </p>
+
+    <p style="margin-top: 4rem">
+      <a href="https://www.buymeacoffee.com/harros" target="_blank"
+        ><img
+          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+          alt="Buy Me A Coffee"
+          style="height: 60px !important; width: 217px !important"
+      /></a>
+    </p>
   `,
 })
 export class ContactComponent {}
